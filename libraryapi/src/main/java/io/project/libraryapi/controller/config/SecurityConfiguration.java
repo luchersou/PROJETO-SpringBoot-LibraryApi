2da8e6eca58b4cbf81b7e07f9123f63e60287b5e
lucash <admin@admin.com>
2025-04-30 11:27:37 -0300

    feat: Add basic auth

---
+++ libraryapi/src/main/java/io/project/libraryapi/controller/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package io.project.libraryapi.controller.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(http -> {
+                    http.anyRequest().authenticated();
+                })
+                .build();
+    }
+}
